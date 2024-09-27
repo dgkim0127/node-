3f5f613f5b8923796b14d6b0e5f444f132f5d954
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-28 05:47:35 +0900

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// Firebase 설정 정보
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
