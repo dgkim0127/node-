d00acf3a633bc6346fe405bd006ebe80401d2ea1
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-28 06:09:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const app = express();
 
 // Firebase 초기화
 const firebaseConfig = {
-  apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
-  authDomain: "jjji-4240b.firebaseapp.com",
-  projectId: "jjji-4240b",
-  storageBucket: "jjji-4240b.appspot.com",
-  messagingSenderId: "876101785840",
-  appId: "1:876101785840:web:6e58681ea9c9780e454a35",
-  measurementId: "G-03999XR4JS"
+    apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
+    authDomain: "jjji-4240b.firebaseapp.com",
+    projectId: "jjji-4240b",
+    storageBucket: "jjji-4240b.appspot.com",
+    messagingSenderId: "876101785840",
+    appId: "1:876101785840:web:6e58681ea9c9780e454a35",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
