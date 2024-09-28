37a0efa000daaf53c150bd99c3f8df662469382c
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-28 10:36:40 +0900

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
+
     // Firebase 설정 정보
   const firebaseConfig = {
     apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
@@ -7,14 +11,10 @@
     messagingSenderId: "876101785840",
     appId: "1:876101785840:web:6e58681ea9c9780e454a35"
 };
-  
-// Firebase 초기화
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
+// Firebase 초기화
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp); // Firestore 초기화
+const storage = getStorage(firebaseApp); // Storage 초기화
 
 export { db, storage };
