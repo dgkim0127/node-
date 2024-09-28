902589fb4bb71a7c722ba5698b878dfb74163f92
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-28 17:01:13 +0900

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-
 import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';  // Auth 추가
 
-    // Firebase 설정 정보
-  const firebaseConfig = {
-    apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
-    authDomain: "jjji-4240b.firebaseapp.com",
-    projectId: "jjji-4240b",
-    storageBucket: "jjji-4240b.appspot.com",
-    messagingSenderId: "876101785840",
-    appId: "1:876101785840:web:6e58681ea9c9780e454a35"
+const firebaseConfig = {
+  apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
+  authDomain: "jjji-4240b.firebaseapp.com",
+  projectId: "jjji-4240b",
+  storageBucket: "jjji-4240b.appspot.com",
+  messagingSenderId: "876101785840",
+  appId: "1:876101785840:web:6e58681ea9c9780e454a35",
+  measurementId: "G-03999XR4JS"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
