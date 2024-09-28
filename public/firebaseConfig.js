7cca97ab527d1aefdb228d4f7ed07aaa0bdccefb
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-28 09:04:21 +0900

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,10 +1,5 @@
-// Firebase 설정 정보
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
-
-// Firebase 설정 정보
-const firebaseConfig = {
+    // Firebase 설정 정보
+  const firebaseConfig = {
     apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
     authDomain: "jjji-4240b.firebaseapp.com",
     projectId: "jjji-4240b",
@@ -12,10 +7,14 @@ const firebaseConfig = {
     messagingSenderId: "876101785840",
     appId: "1:876101785840:web:6e58681ea9c9780e454a35"
 };
-
-// Firebase 초기화
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp); // Firestore 초기화
-const storage = getStorage(firebaseApp); // Storage 초기화
-
-export { db, storage };
+  
+  // Firebase 초기화
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+  import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+  
+  export { db, storage };
