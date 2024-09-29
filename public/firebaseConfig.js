bc73e8d06c2c56f894e2cfc4b5b3e621782bb8e2
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-30 03:02:45 +0900

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -4,12 +4,13 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.
 import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
 
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
+  apiKey: "AIzaSyDFysg8I_qKtDqDJLWg1_npTPBWRMM_5WY",
+  authDomain: "jjji-4240b.firebaseapp.com",
+  projectId: "jjji-4240b",
+  storageBucket: "jjji-4240b.appspot.com",
+  messagingSenderId: "876101785840",
+  appId: "1:876101785840:web:6e58681ea9c9780e454a35",
+  measurementId: "G-03999XR4JS"
 };
 
 const app = initializeApp(firebaseConfig);
