9f36f78863f3de54776bc6ad2cd6e4775198d046
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-30 01:17:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const PORT = process.env.PORT || 3000;
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 기본 경로로 접속 시 로그인 페이지 제공
+// 루트 경로로 들어오면 로그인 페이지로 리디렉션
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    res.sendFile(path.join(__dirname, 'public', 'login.html')); // login.html로 리디렉션
 });
 
-// 회원가입 페이지
+// 회원가입 페이지 라우트
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
 
-// 메인 페이지 (로그인 후 리디렉션될 페이지)
+// 메인 페이지 라우트
 app.get('/main', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
