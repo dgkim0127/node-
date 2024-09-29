e9af966bdc9404cf6421121987a1bea119812739
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-30 00:50:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 app.listen(PORT, () => {
