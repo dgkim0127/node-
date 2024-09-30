c9c6677beab68ccc31fd03640914649e0584f333
dgkim0127 <119631573+dgkim0127@users.noreply.github.com>
2024-09-30 09:45:51 +0900

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,11 +1,6 @@
 import { db } from './firebaseConfig.js';
 import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
 
-// 업로드 버튼 클릭 시 페이지 이동
-document.getElementById('uploadButton').addEventListener('click', function() {
-    window.location.href = 'upload.html';  // 올바른 경로로 설정
-});
-
 const postContainer = document.querySelector('.posts');
 
 // Firestore에서 게시물 데이터를 불러오기
@@ -17,19 +12,13 @@ async function loadPosts() {
         const postElement = document.createElement('div');
         postElement.classList.add('post');
 
-        // 썸네일 이미지가 있는지 확인하고 표시
+        // 썸네일만 표시
         const thumbnailURL = postData.thumbnailURL;
 
         if (thumbnailURL) {
             postElement.innerHTML = `
                 <a href="detail.html?id=${doc.id}">
                     <img src="${thumbnailURL}" alt="썸네일 이미지">
-                </a>
-            `;
-        } else {
-            postElement.innerHTML = `
-                <a href="detail.html?id=${doc.id}">
-                    <p>이미지가 없습니다</p>
                 </a>
             `;
         }
